864becb2da2e5c146baa8c36501c70797e612ca9
JaeBuhmJO <favor99531@gmail.com>
2023-06-15 23:24:34 +0900

    인덱스 페이지 접근 가능한 최소 포맷

---
+++ shoes/src/main/java/com/project/ShoesApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoesApplication.class, args);
+	}
+
+}
---
+++ shoes/src/main/java/com/project/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		log.info("Welcome shop!");
+		return "index";
+	}
+	
+}
